fded8cb4aaf94c26f78a4cc3ff271e38e98a8851
Guannan Wei <kiss.kraks@gmail.com>
2016-03-17 17:06:29 -0600

    add syntax

---
+++ syntax.rkt
@@ -0,0 +1,35 @@
+#lang racket
+
+#|
+
+<top> ::= <def>* <exp>*
+
+<def> ::= (define [<var> : <type>] <exp>)
+
+<exp> ::= <var>
+       |  <literal>
+       |  <prim>
+       |  (quote <literal>)
+       |  (λ <label> [<var> : <type>] : <type> <exp>)
+       |  (if <exp> <exp> <exp>)
+       |  (let ([<var> : <type>] <exp>) <body>)
+       |  (<exp> <exp>)
+
+<literal> ::= int | bool | symbol
+<prim> ::= + | - | * | / | >= | <= | > | < | = | and | or | not
+
+<label> ::= symbol
+=============
+
+<type> ::= int | bool | symbol
+         | (-> <type> <type>)
+         | (int <predicate>) | (bool <predicate>) | (symbol <predicate>)
+
+<predicate> ::= #t | (<type-op> <operand> <operand>)
+<type-op> ::= > | < | = | >= | <= | and | or | not
+<operand> ::= <predicate> | <literal> | <var>
+
+|#
+
+(λ 'plus1 [x : (int (>= x 0))] : [y : (int (> y x))]
+  (+ x 1))
